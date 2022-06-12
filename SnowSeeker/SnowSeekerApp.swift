f5aaf98d9cf28967f18ad507bf3123c6a6d602d6
Michael Pascucci <mdpascucci@live.com>
2022-06-12 07:26:33 -0400

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Michael & Diana Pascucci on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
